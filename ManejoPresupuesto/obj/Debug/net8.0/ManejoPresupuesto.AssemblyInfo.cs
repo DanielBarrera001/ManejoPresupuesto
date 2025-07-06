470b54d3731cc2abe4ce3d43adf10d7c4358f91e
Daniel <barreraquezadadanielalejandro@gmail.com>
2025-07-06 11:15:24 -0600

    Query y vista para indice de cuentas

--- ManejoPresupuesto/obj/Debug/net8.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/obj/Debug/net8.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebd1b727992c3b351939b61ab7e1d23a4f64cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc54e4a23ad9a119041aee55c892d3f08ef5963")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
